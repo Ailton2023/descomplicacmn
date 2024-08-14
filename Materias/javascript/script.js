63cfecd1c6443da3f9a3c6c5a0024f5125e51df8
Ailton2023 <141515952+Ailton2023@users.noreply.github.com>
2024-08-14 11:21:01 -0300

    eryery
    yyyyy

---
+++ Materias/javascript/script.js
@@ -0,0 +1,8 @@
+const tabs = document.querySelectorAll('.tab-btn');
+tabs.forEach(tab => tab.addEventListener('click', ()=>tabClicked(tab)));
+const tabClicked = (tab) =>{
+    const contents = document.querySelectorAll('.content') 
+    contents.forEach(content => content.classList.remove('show'));
+    const contentId = tab.getAttribute('content-id');
+    console.log(content);
+}
